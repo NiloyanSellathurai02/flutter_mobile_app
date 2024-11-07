7add30eb969c2e86df7fc99ebdbf49fcf3ed3cf8
Niloyan Sellathurai <n.sella2001@gmail.com>
2024-11-07 21:56:49 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              const Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98)
+            ]),
+          ),
+          child: const Center(
+            child: Text('Welcome!'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
