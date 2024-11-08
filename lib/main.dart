9b596c9622ef5d17d6f3c13deed43845825bb0c3
Niloyan Sellathurai <n.sella2001@gmail.com>
2024-11-08 18:03:35 +0100

    changed the text color and the background color

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,21 @@ void main() {
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(colors: [
-              const Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 26, 2, 80),
               Color.fromARGB(255, 45, 7, 98)
-            ]),
+            ],
+            begin:Alignment.topLeft,
+            end:Alignment.bottomRight,
+            ),
           ),
           child: const  Center(
-            child: Text('Welcome!'),
-          ),
+            child: Text('Hello!' , style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+          ),),
         ),
       ),
     ),
+  ),
   );
 }
