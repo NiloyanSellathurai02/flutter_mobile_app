24f3b1bc1fea9e38c6300d1734fab6a7b90b05be
Niloyan Sellathurai <n.sella2001@gmail.com>
2024-11-10 19:36:30 +0100

    seperated some code in seperated files

---
+++ lib/styled_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText({super.key});
+  @override
+  build(context){
+    return const Text('Hello!' , style: TextStyle(
+              color: Colors.white,
+              fontSize: 28,
+          ),);
+  }
+}
+
+
+
+
+
+
